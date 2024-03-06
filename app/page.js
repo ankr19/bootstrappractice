903f9b2fe5102a1229641a37be8ae27497b0cef1
Anand Kumar <ankm335@gmail.com>
2024-03-06 14:01:09 +0530

    ui: practicing the sidebar with nextjs

--- app/page.js
+++ app/page.js
@@ -1,25 +1,11 @@
-"use client";
-import Box from "@/components/Box";
-import Grid from "@/components/Grid";
-import { Fade, Roll, Slide } from "react-swift-reveal";
+import Accordion from "@/components/Accordion";
+import Modal from "@/components/Modal";
 
 export default function Home() {
   return (
     <>
-      <Grid />
-      <Fade duration={1000} delay={400} distance="30px">
-        <Box />
-      </Fade>
-      <Box />
-      <Box />
-      <Slide up duration={4000} delay={500}>
-        <Box />
-      </Slide>
-      <div className="row">
-        <div className="col-2 img-container pl-5">
-          <img className="w-50" src="/mov1.png" alt="...." />
-        </div>
-      </div>
+    <Accordion/>
+    <Modal/>
     </>
   );
 }
---
+++ components/BootstrapJs.jsx
@@ -0,0 +1,8 @@
+"use client"
+import React from 'react'
+export default function BootstrapJs() {
+    React.useEffect(()=>{
+import('bootstrap/dist/js/bootstrap.min.js')
+    },[])
+  return null
+}
