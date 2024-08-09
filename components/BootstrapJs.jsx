36579f9ede58b5c5ba8b3ac8b349fac832781c84
Anand Kumar <ankm335@gmail.com>
2024-08-09 15:51:49 +0530

    updated files

--- components/BootstrapJs.jsx
+++ components/BootstrapJs.jsx
@@ -1,8 +1,8 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 export default function BootstrapJs() {
-    React.useEffect(()=>{
-import('bootstrap/dist/js/bootstrap.min.js')
-    },[])
-  return null
+  React.useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.min.js");
+  }, []);
+  return null;
 }
